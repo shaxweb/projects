f3d2e6cc5ca305e77849cbb1e4822942222a2aff
shaxweb <igrayunagitare@gmail.com>
2025-06-11 17:04:51 +0500

    update

--- WebAppTG/FirstTestApp/script.js
+++ WebAppTG/FirstTestApp/script.js
@@ -1,10 +1,14 @@
 const db = localStorage
 let numberView = document.getElementById("number_view")
-
-
  
 const clickUp = () => {
   let number = JSON.parse(db.getItem("number")) || {number: 0};
   db.setItem("number", JSON.stringify({number: number.number + 1}))
   numberView.innerText = `Clicked: ${number.number+1}`
+}
+
+const resetDown = () => {
+  let number = JSON.parse(db.getItem("number")) || {number: 0};
+  db.setItem("number", JSON.stringify({number: 0}))
+  numberView.innerText = `Clicked: 0`
 }
