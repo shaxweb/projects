7fdf04d9fd94e61c0b7d8d6d1039df044366a9e3
shaxweb <igrayunagitare@gmail.com>
2025-06-13 12:06:18 +0500

    update app.py

--- app.py
+++ app.py
@@ -5,22 +5,13 @@ app = Flask(__name__)
 render = render_template
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
-    current_time = time.strftime("%H:%M:%S", time.localtime())
-	return render("app.py", time=current_time)
+    if request.method == "POST":
+        pass
+    else:
+        current_time = time.strftime("%H:%M:%S", time.localtime())
+        return render("app.py", time=current_time)
 
-# Пример GET-запроса
-@app.route("/hello", methods=['GET'])
-def hello():
-    return jsonify({"message": "Hello, world!"})
-
-# Пример POST-запроса
-@app.route('/echo', methods=['POST'])
-def echo():
-    data = request.json  # Получаем JSON из тела запроса
-    return jsonify({"you_sent": data})
-
-# Запуск приложения
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True)
